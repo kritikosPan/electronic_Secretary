b4f0ef9fb35c831c25f73ccfddfdcac95156cedd
EyesCratcher <spirosc90@gmail.com>
2022-08-05 22:01:10 +0300

    Add GenericRepository

---
+++ Repository/Core/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IGenericRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(object id);
+
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+
+        T SingleOrDefault(Expression<Func<T, bool>> predicate);
+
+        void Create(T obj);
+
+        void Update(T obj);
+
+        void Delete(object id);
+
+        void Save();
+    }
+}
