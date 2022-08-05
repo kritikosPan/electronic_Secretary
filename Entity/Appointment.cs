f9a87f3213020414d324bea1240378400b8f8057
EyesCratcher <spirosc90@gmail.com>
2022-08-05 14:45:17 +0300

    Add project files.

---
+++ Entity/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public DateTime DateofAppointment { get; set; }
+
+        //Navigation Properties
+
+        public User User { get; set; }
+
+    }
+}
---
+++ MyDatabase/Initializers/MockupDbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDatabase.Initializers
+{
+    public class MockupDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
